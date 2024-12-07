230ed7a42024c206ad6ffddca24900bf3708afdb
Vlad Kovdria <kovdryavlad@gmail.com>
2024-12-07 19:09:06 +0200

    End of section 2

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,123 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(
+        AuctionDbContext auctionDbContext,
+        IMapper mapper)
+    {
+        _dbContext = auctionDbContext;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions() 
+    {
+        var auctions = await _dbContext.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item.Make)
+            .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _dbContext.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null) 
+        {
+            return NotFound();
+        }
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto) 
+    {
+        var auction = _mapper.Map<Auction>(auctionDto);
+
+        //TODO: add current user as seller
+        auction.Seller = "test";
+
+        _dbContext.Auctions.Add(auction);
+
+        var result = await _dbContext.SaveChangesAsync() > 0;
+
+        if (!result) 
+        {
+            return BadRequest("Could not save changes to the DB");
+        }
+
+        return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _dbContext.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null) 
+        {
+            return NotFound();
+        }
+
+        //TODO: check seller == username
+
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+
+        var result = await _dbContext.SaveChangesAsync() > 0;
+
+        if (!result) 
+        {
+            return BadRequest("Could not update entity in DB");
+        }
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id) 
+    {
+        var auction = _dbContext.Auctions.Find(id);
+
+        if (auction == null) 
+        {
+            return NotFound();
+        }
+
+        //TODO: check seller == username
+        _dbContext.Auctions.Remove(auction);
+
+        var result = await _dbContext.SaveChangesAsync() > 0;
+
+        if (!result) 
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
+}
